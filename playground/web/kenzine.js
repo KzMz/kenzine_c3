28950082f35dd524b2c1eb735c186b27b01476f2
Mattia Bruno <mattia.bruno.kzmz@gmail.com>
2024-11-04 01:57:02 +0100

    memory, dyn states, game handlers

--- playground/web/kenzine.js
+++ playground/web/kenzine.js
@@ -1,13 +1,39 @@
-async function getKenzineWasm() {
-    return await WebAssembly.instantiateStreaming(fetch('playground.wasm'), {
-        "kenzine": {}
+function make_env(...envs) {
+    return new Proxy(envs, {
+        get(target, prop, receiver) {
+            for (const env of envs)
+            {
+                if (env.hasOwnProperty(prop))
+                    return env[prop];
+            }
+            return (...args) => { console.error(`No such property: ${prop}`)}
+        }
     });
 }
 
+function log(level, format, ...args)
+{
+    console.log(wasm);
+    console.log(`[${level}] ${format}`);
+}
+
+const imports = {
+    log: (level, format, ...args) => log(level, format, ...args)
+};
+
+async function getKenzineWasm() {
+    return await WebAssembly.instantiateStreaming(fetch('playground.wasm'), {
+        "env": make_env(imports)
+    });
+}
+
+let wasm;
+
 (async () => {
-    const [wasm] = await Promise.all([
+    const instance = await Promise.all([
         getKenzineWasm()
     ]);
-    console.log(wasm);
-    console.log(wasm.instance.exports.sum(1, 2));
+
+    wasm = instance[0];
+    wasm.instance.exports.main();
 })();
