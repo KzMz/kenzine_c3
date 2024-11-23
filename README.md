7563593efb4be24ddcafa761459f15edc92d4c61
Mattia Bruno <mattia.bruno.kzmz@gmail.com>
2024-11-23 01:07:40 +0100

    some changes (especially on dynamic allocations) to better handle wasm / first wasm triangle!

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Just run "build.bat" for native build and build-web.bat for web build
 
 ## TODO
 - [x] Resource Embed
-- [ ] First wasm triangle!
+- [x] First wasm triangle!
 - [ ] Material asset definition and loader
 - [ ] Test hard-coded 3d rendering
 - [ ] Geometry asset definition and loader
--- playground/web/kenzine.js
+++ playground/web/kenzine.js
@@ -30,6 +30,7 @@ class Kenzine {
         this.device = undefined;
         this.encoder = undefined;
         this.pass = undefined;
+        this.pipeline = undefined;
         this.clearColor = { r: 0.0, g: 0.0, b: 0.0, a: 1.0 };
 
         this.canvas = document.getElementById(canvas_id);
@@ -42,12 +43,25 @@ class Kenzine {
         this.wasm.instance.exports.main();
     }
 
+    load_resource(type, name)
+    {
+        const name_ptr = this.wasm.instance.exports.allocate(name.length);
+        const name_array = new Uint8ClampedArray(this.wasm.instance.exports.memory.buffer, name_ptr, name.length);
+
+        for (let i = 0; i < name.length; i++) {
+            name_array[i] = name.charCodeAt(i);
+        }
+        name_array[name.length] = 0;
+        
+        return this.wasm.instance.exports.load_resource(type, name_ptr, name_array.length - 1);
+    }
+    
     getImports() {
         return {
             wasm_write: (buffer, len) => {
                 console.log(read_string_from_memory(this.wasm, buffer, len));
             },
-            wasm_load_resource: async (buffer, len) => {
+            /*wasm_load_resource: async (buffer, len) => {
                 const path = read_string_from_memory(this.wasm, buffer, len);
 
                 const response = await fetch(path);
@@ -58,7 +72,7 @@ class Kenzine {
                 data.set(array);
 
                 this.wasm.instance.exports.on_resource_loaded(data_ptr, array.length);
-            },
+            },*/
             app_set_title: (buffer, len) => {
                 this.title = read_string_from_memory(this.wasm, buffer, len);
             },
@@ -151,6 +165,49 @@ class Kenzine {
 
         this.context = context;
 
+        // TEMP
+        const resource = this.load_resource(4, "builtin");
+        const code_size = this.wasm.instance.exports.shader_get_code_size(resource);
+        const code_ptr = this.wasm.instance.exports.shader_get_code(resource);
+        const code = read_string_from_memory(this.wasm, code_ptr, code_size);
+        
+        const shaderModule = this.device.createShaderModule({
+            "code": code
+        });
+
+        // Vertex
+        let entry_size = this.wasm.instance.exports.shader_get_entry_size(resource, 0);
+        let entry_ptr = this.wasm.instance.exports.shader_get_entry(resource, 0);
+        let entry = read_string_from_memory(this.wasm, entry_ptr, entry_size);
+        const vertex = {
+            module: shaderModule,
+            entryPoint: entry,
+            buffers: []
+        };
+
+        // Fragment
+        entry_size = this.wasm.instance.exports.shader_get_entry_size(resource, 1);
+        entry_ptr = this.wasm.instance.exports.shader_get_entry(resource, 1);
+        entry = read_string_from_memory(this.wasm, entry_ptr, entry_size);
+        const fragment = {
+            module: shaderModule,
+            entryPoint: entry,
+            targets: [
+                {
+                    format: canvasFormat
+                }
+            ]
+        };
+
+        this.pipeline = this.device.createRenderPipeline({
+            layout: "auto",
+            vertex: vertex,
+            fragment: fragment,
+            primitive: {
+                topology: "triangle-list",
+            }
+        });
+
         window.requestAnimationFrame((timestamp) => {
             this.previous_timestamp = timestamp;
             window.requestAnimationFrame(this.frame.bind(this));
@@ -162,7 +219,8 @@ class Kenzine {
     }
 
     async draw_frame(delta_time) {
-
+        this.pass.setPipeline(this.pipeline);
+        this.pass.draw(3, 1, 0, 0);
     }
 
     async end_frame(delta_time) {
